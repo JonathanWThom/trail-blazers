cc4b78c6e04978c7f92852ec1e2007b59d504041
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-27 15:59:10 -0800

    Add key to gitignore

--- src/app/stats.service.ts
+++ src/app/stats.service.ts
@@ -3,16 +3,17 @@ import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import { nbaKey } from './api-keys';
 
 @Injectable()
   export class StatsService {
-    private statsUrl = 'https://api.sportradar.us/nba-t3/seasontd/2016/REG/standings.json?api_key=heqgxh8yv5q2suv2yjrrynr3';  // URL to web API
+    private statsUrl = 'https://api.sportradar.us/nba-t3/seasontd/2016/REG/standings.json?api_key=' + nbaKey.apiKey;
     constructor (private http: Http) {}
 
     getStandings() {
       return this.http.get(this.statsUrl)
       .map(res => {
-      return <any[]> res.json(); //return mapped object from here
+      return <any[]> res.json();
     });
   }
 }
