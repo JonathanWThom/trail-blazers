98e761939d400a616e9957ab781c70a42b4e8b8a
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-27 09:07:12 -0800

    will display player images

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,3 +1,3 @@
 export class Player {
-  constructor (public name: string, public position: string, public number: number, public height: number, public experience: number, public: image: string) { }
+  constructor (public name: string, public position: string, public number: number, public height: number, public experience: number, public image: string) { }
 }
