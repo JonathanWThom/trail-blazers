da909ab11f661b71caf1bfe900ff93347cfe06ce
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-26 22:25:08 -0800

    create pipe method

--- src/app/position.pipe.ts
+++ src/app/position.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { FirebaseListObservable } from 'angularfire2';
 
 @Pipe({
   name: 'position',
@@ -6,8 +7,34 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class PositionPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
-  }
+  //change to any object types if neccessary
 
+  transform(input: any[], position: string) {
+    var output: FirebaseListObservable<any[]>;
+
+    if (position === "Center") {
+      input.forEach(function(player) {
+        if (player.position === "Center") {
+          output.push(player);
+        }
+      });
+      return output;
+    } else if (position === "Forward") {
+      input.forEach(function(player) {
+        if (player.position === "Forward") {
+          output.push(player);
+        }
+      });
+      return output;
+    } else if (position === "Guard") {
+      input.forEach(function(player) {
+        if (player.position === "Guard") {
+          output.push(player);
+        }
+      });
+      return output;
+    } else {
+      return input;
+    }
+  }
 }
