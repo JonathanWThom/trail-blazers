010e581b876a2a9a311deb940b2b68f8c84d67d2
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-26 21:03:25 -0800

    Delete api-keys.ts

--- src/app/api-keys.ts
+++
@@ -1,7 +0,0 @@
-export var masterFirebaseConfig = {
-  apiKey: "AIzaSyDdwHzo7b3q3HMRomSL1jRsJ7UEELDz_vM",
-  authDomain: "trailblazers-e4317.firebaseapp.com",
-  databaseURL: "https://trailblazers-e4317.firebaseio.com",
-  storageBucket: "trailblazers-e4317.appspot.com",
-  messagingSenderId: "632657475929"
-};
